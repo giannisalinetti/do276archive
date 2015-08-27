7fb451be502e6e89b64e8a7e8cfa6eb5de29a613
Jim Rigsbee <jimrigsbee@gmail.com>
2015-08-27 17:35:10 -0400

    Got compose/linked working with wait for EAP to come up after mysql started

--- apps/jee/compose/README.md
+++ apps/jee/compose/README.md
@@ -1,1 +1,9 @@
 # Running Java EE To Do App with Docker Compose
+Install Red Hat Software Collections pip
+sudo yum -y install python27 python27-python-pip
+
+Install docker compose:
+sudo scl enable python27 "pip install docker-compose"
+
+scl enable python27 "docker-compose up"
+
--- apps/jee/linked/run.sh
+++ apps/jee/linked/run.sh
@@ -1,7 +1,4 @@
-# Java EE To Do List
-
-The following commands run the application as linked containers.
-
+#!/bin/sh 
 docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 mysql
 
 docker run -d -e MYSQL_DB_NAME=items --link mysql:mysql --name eap -p 8080:8080 todojee
