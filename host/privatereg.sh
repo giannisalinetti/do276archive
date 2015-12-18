7567f034713275301f1d9a7c32b16bc9d7238d4d
jrigsbee <jrigsbee@redhat.com>
2015-12-17 19:46:53 -0500

    added private registry setup

---
+++ host/privatereg.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+HOSTNAME=`hostname`
+
+yum install -y docker-registry
+
+#openssl genrsa -out /etc/pki/tls/private/self.key 1024
+
+#openssl req -new -key /etc/pki/tls/private/self.key -x509 -days 30828 -out /etc/pki/tls/certs/self.crt -subj "/C=US/ST=NC/L=Training/O=example.com/CN=$HOSTNAME/emailAddress=root@example.com"
+
+
+#cp /usr/lib/systemd/system/docker-registry.service /etc/systemd/system/
+#find /etc/systemd/system/docker-registry.service -type f -exec sed -i "s/ExecStart=\/usr\/bin\/gunicorn/ExecStart=\/usr\/bin\/gunicorn --certfile \/etc\/pki\/tls\/certs\/self.crt --keyfile \/etc\/pki\/tls\/private\/self.key/g" {} +
+
+
+find /etc/docker-registry.yml -type f -exec sed -i "0,/_env:SEARCH_BACKEND/ s/_env:SEARCH_BACKEND/_env:SEARCH_BACKEND:sqlalchemy/" {} +
+ 
+systemctl enable docker-registry
+systemctl start docker-registry
+#systemctl daemon-reload
+systemctl enable firewalld
+systemctl start firewalld
+firewall-cmd --zone=public --add-port=5000/tcp --permanent
+firewall-cmd --reload
+
+#Trusting the certificate file
+
+#yum -y install ca-certificates
+#update-ca-trust enable
+#cp /etc/pki/tls/certs/self.crt /etc/pki/ca-trust/source/anchors/
+#update-ca-trust extract
+
+
+#To rebuild the internal registry DB
+
+systemctl stop docker
+systemctl stop docker-registry
+mkdir -p /var/lib/docker-registry
+rm -rf /tmp/docker-registry.db
+systemctl start docker
+systemctl start docker-registry
+
