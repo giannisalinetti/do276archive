2eba668f343904c954558d83cf80ca8e016cc8a7
zgutterm@redhat.com <you@example.com>
2015-12-18 14:18:19 -0500

    fix build script for python_api

--- apps/python_api/build.sh
+++ apps/python_api/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
-docker build -t do276/todoapi_python .
-
+rm -fr build
+mkdir -p build
+cp -a ../../apps/python_api/* build
+cd build
+docker build -t do276/todoapi_python -f ../Dockerfile .
