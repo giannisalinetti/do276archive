9c024688bb49c3e024cff8c8bd3d645f0cfad85a
Fernando Lozano <flozano@redhat.com>
2015-12-18 21:52:51 -0200

    added docker.io mysql and httpd images to the list to be loaded into the private registry

--- host/up-with-contents.sh
+++ host/up-with-contents.sh
@@ -8,14 +8,21 @@
 
 SVN_ARTIFACTS=/svn/training/DO276_Container_Apps/artifacts
 IMAGESDIR=$HOME
-IMAGES="rhel7 openshift.mysql-55-rhel7 do276.mysql-55-rhel7"
+# mysql and httpd are from docker.io
+IMAGES="mysql httpd rhel7 openshift.mysql-55-rhel7 do276.mysql-55-rhel7"
 HOSTDIR=$(pwd)
+
+$POWEROFF=$(vagrant status | grep workstation | grep poweroff)
+if [ "$POWEROFF" != "" ]; then
+  echo "Needs to be run with the workstaton box off." 1>&2
+  exit 1
+fi
 
 #svn update
 pushd $SVN_ARTIFACTS
 tar czf $HOSTDIR/contents.tar.gz content
-#cp do276.mysql-55-rhel7-src.tar.gz $HOSTDIR
 popd
+
 for image in $IMAGES ; do
   cp -p $IMAGESDIR/$image.tar.gz .
 done
@@ -24,8 +31,11 @@ vagrant up --provider=virtualbox
 
 vagrant ssh -- sudo tar xzf /vagrant/contents.tar.gz -C /
 
+# Erros during those rm/rmi are expected if the box is brand new or was properly cleaned up
+vagrant ssh -- "docker rm -f \$(docker ps -qa)"
+vagrant ssh -- "docker rmi -f \$(docker images -q)"
 vagrant ssh -- "cd /vagrant ; for i in $IMAGES ; do docker load -i \$i.tar.gz ; done"
-vagrant ssh -- "for i in \$(docker images | awk '{print \$1}' | grep -v REPOSITORY | sed 's/registry.access.redhat.com\///'); do docker push \$i; done"
+vagrant ssh -- "for i in \$(docker images | awk '{print \$1}' | grep -v REPOSITORY | sed 's/registry.access.redhat.com\///' | sed 's/docker.io\///'); do docker push \$i; done"
 vagrant ssh -- "docker rmi -f \$(docker images -q)"
 
 vagrant ssh -- -l student
