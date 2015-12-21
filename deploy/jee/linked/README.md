90dd5ec527acfcb2cd1c4aa96506ddf4afd8b9e9
Fernando Lozano <flozano@redhat.com>
2015-12-21 14:41:05 -0500

    changed deploy/jee to use custom MySQL image

--- deploy/jee/linked/README.md
+++ deploy/jee/linked/README.md
@@ -1,7 +1,4 @@
 # Java EE To Do List
 
-The following commands run the application as linked containers.
+To test the application, access http://localhost:30080/todo from your developer workstation (the box host, NOT the workstation VM)
 
-docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 mysql
-
-docker run -d -e MYSQL_DB_NAME=items --link mysql:mysql --name eap -p 8080:8080 todojee
--- deploy/jee/linked/run.sh
+++ deploy/jee/linked/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh 
-if [ ! -d "data" ]; then
+if [ ! -d "work" ]; then
   echo "Create database volume..."
-  mkdir -p data initdb
-  chcon -Rt svirt_sandbox_file_t data initdb
-  chmod o+rwx data
-  cp db.sql initdb 
+  mkdir -p work/init
+  cp db.sql work/init
+  sudo chcon -Rt svirt_sandbox_file_t work
+  sudo chown -R 27:27 work
 else
-  rm -fr initdb/*
+  sudo rm -fr work/init
 fi
 
-docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -v `pwd`/data:/var/lib/mysql -v `pwd`/initdb:/docker-entrypoint-initdb.d -p 30306:3306 mysql
+docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -v $PWD/work:/var/lib/mysql -p 30306:3306 do276/mysql-55-rhel7
 
-docker run -d -e MYSQL_DB_NAME=items --link mysql:mysql --name eap -p 30080:8080 todojee
+docker run -d -e MYSQL_DB_NAME=items --link mysql:mysql --name todo -p 30080:8080 do276/todojee
