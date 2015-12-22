6ce38384b82b7d2cfae644a4d9b532ab848bbb37
Fernando Lozano <flozano@redhat.com>
2015-12-22 11:12:31 -0500

    added data dir contents to custom mysql image test

--- images/mysql/test.sh
+++ images/mysql/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 
 sudo rm -rf data init
-mkdir -p data init
-cp -p test/testdata.sql init
-sudo chcon -Rt svirt_sandbox_file_t data init
-sudo chown -R 27:27 data init
+mkdir -p work/data work/init
+cp -p test/testdata.sql work/init
+sudo chcon -Rt svirt_sandbox_file_t work
+sudo chown -R 27:27 work
 
 docker run -d --name test-mysql -p 30306:3306 \
  -e MYSQL_USER=testuser -e MYSQL_PASSWORD=secret -e MYSQL_DATABASE=contacts \
- -v $PWD/data:/var/lib/mysql/data \
- -v $PWD/init:/var/lib/mysql/init \
+ -v $PWD/work/data:/var/lib/mysql/data \
+ -v $PWD/work/init:/var/lib/mysql/init \
  do276/mysql-55-rhel7
 sleep 9
 source  /opt/rh/mysql55/enable
@@ -17,4 +17,7 @@ source  /opt/rh/mysql55/enable
 mysqlshow -P30306 -h127.0.0.1 -utestuser -psecret contacts
 # Expected result is single row for "John Doe"
 mysql -P30306 -h127.0.0.1 -utestuser -psecret  contacts < test/query.sql
-
+# Expected result is ibdata and ib_logfile* files under data
+ls work/data
+# Expected result is contacts.frm and db.opt files
+sudo ls work/data/contacts
