96162980b9e4b40c0893902823820131fc757869
zgutterm@redhat.com <you@example.com>
2015-12-22 09:08:58 -0500

    revert run scripts

--- deploy/python_api/linked/run_debug.sh
+++ deploy/python_api/linked/run_debug.sh
@@ -1,17 +1,15 @@
 #!/bin/sh 
-#!/bin/sh 
 if [ ! -d "work" ]; then
   echo "Create database volume..."
-  mkdir -p work/init
+  mkdir -p work/init work/data
   cp db.sql work/init 
+  sudo chown -R 27:27 work
+  sudo chcon -Rt svirt_sandbox_file_t work
 else
-  sudo rm -rf work/*
+  sudo rm -fr work/init
 fi
 
-sudo chown -R 27:27 work
-sudo chcon -Rt svirt_sandbox_file_t work
-
-docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -v $PWD/work:/var/lib/mysql -p 30306:3306 do276/mysql-55-rhel7
+docker run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -v $PWD/work/data:/var/lib/mysql/data -v $PWD/work/init:/var/lib/mysql/init -p 30306:3306 do276/mysql-55-rhel7
 
 docker run -d --name=todoui -p 30000:80 do276/todo_frontend
 
