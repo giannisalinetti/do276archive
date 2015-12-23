e169088b01e1ceb5c823684f678e980d505cfd6b
zgutterm@redhat.com <you@example.com>
2015-12-23 12:48:22 -0500

    compose build

--- images/php/compile.sh
+++ images/php/compile.sh
@@ -6,14 +6,7 @@ cd todo/api
 echo $PWD
 if [ -f composer.json ]; then
   echo "Found 'composer.json', installing dependencies using composer.phar... "
-
-  # Install Composer
-  php -r "readfile('https://getcomposer.org/installer');" | php
-
-  # Install App dependencies using Composer
-  ./composer.phar install --no-interaction --no-ansi --no-scripts --optimize-autoloader
-
-  if [ ! -f composer.lock ]; then
-    echo -e "\nConsider adding a 'composer.lock' file into your source repository.\n"
+  if [ -f composer.phar ]; then
+    php composer.phar install
   fi
 fi
