a0978381499ea831aba6fb8323591992e9679dc1
zgutterm@redhat.com <you@example.com>
2015-12-23 13:12:08 -0500

    fix build compile

--- images/php/compile.sh
+++ images/php/compile.sh
@@ -2,11 +2,16 @@
 
 set -e
 echo "Attempting to run compose in the following directory..."
-cd todo/api
-echo $PWD
 if [ -f composer.json ]; then
   echo "Found 'composer.json', installing dependencies using composer.phar... "
+
+  # Install Composer
+  php -r "readfile('https://getcomposer.org/installer');" | php
+
+  # Install App dependencies using Composer
+  ./composer.phar install --no-interaction --no-ansi --no-scripts --optimize-autoloader
+
   if [ -f composer.phar ]; then
-    php composer.phar install
+    echo -e "\nConsider adding a 'composer.lock' file into your source repository.\n"
   fi
 fi
