035ccadf539df7f669f5359c5bf45090c8d7d11d
vagrant <vagrant@workstation.lab.example.com>
2015-12-23 11:07:43 -0500

    tested and debugged python combo on kubernetes

--- deploy/python/build.sh
+++ deploy/python/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm -fr build
 mkdir -p build
+# docker needs to read all subdirs - get rid of SQL db
+sudo rm -fr linked/work
 cp -a ../../apps/python/* build
 docker build -t do276/todopython .
