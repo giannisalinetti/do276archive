c26a9ffebbe88e1b0392e4f9fa1edffb9685d29d
Fernando Lozano <flozano@redhat.com>
2015-12-31 14:45:40 -0500

    nodejs api ok linked and kubernetes

--- deploy/nodejs_api/build.sh
+++ deploy/nodejs_api/build.sh
@@ -2,13 +2,12 @@
 
 rm -fr build
 mkdir -p build
-cp -ap ../../apps/nodejs/* build
+cp -ap ../../apps/nodejs_api/* build
 rm build/*.sh
-cp -p ../../apps/nodejs_api/models/db.js build/models
 
 # base image ONBUILD does npm install
 
 # docker build complains if he cannot read the database folder even if not needed for building the image
 sudo rm -rf {linked,kubernetes}/work
 
-docker build -t do276/todonodejs .
+docker build -t do276/todoapi_nodejs .
