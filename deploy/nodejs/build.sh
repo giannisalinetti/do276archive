5ab9dc89cfbb90cac880eea7cfe8c42ce0eaa2e2
Fernando Lozano <flozano@redhat.com>
2015-12-31 13:03:37 -0500

    nodejs combo working with docker links

---
+++ deploy/nodejs/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -fr build
+mkdir -p build
+cp -ap ../../apps/nodejs/* build
+rm build/*.sh
+cp -p ../../apps/nodejs_api/models/db.js build/models
+
+# base image ONBUILD does bundle install
+
+# docker build complains if he cannot read the database folder even if not needed for building the image
+sudo rm -rf {linked,kubernetes}/work
+
+docker build -t do276/todonodejs .
