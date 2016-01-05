696fa435a53ddadf6240d3c73041bfe800cc06c2
zgutterm@redhat.com <you@example.com>
2016-01-05 11:35:50 -0500

    add compile code into run script

--- deploy/php/run.sh
+++ deploy/php/run.sh
@@ -18,4 +18,23 @@ export PHP_INI_SCAN_DIR=${PHP_INI_SCAN_DIR:-/etc/opt/rh/rh-php56/php.d}
 envsubst < /opt/app-root/etc/php.ini.template > $PHPRC
 envsubst < /opt/app-root/etc/php.d/10-opcache.ini.template > $PHP_INI_SCAN_DIR/10-opcache.ini
 
+echo "Current directory ${PWD}"
+cd todo/api
+echo "After ${PWD}"
+
+if [ -f composer.json ]; then
+  echo "Found 'composer.json', installing dependencies using composer.phar... "
+
+  # Install Composer
+  php -r "readfile('https://getcomposer.org/installer');" | php
+
+  # Install App dependencies using Composer
+  ./composer.phar install --no-interaction --no-ansi --no-scripts --optimize-autoloader
+
+  if [ -f composer.phar ]; then
+    echo -e "\nConsider adding a 'composer.lock' file into your source repository.\n"
+  fi
+fi
+
+
 exec httpd -D FOREGROUND
