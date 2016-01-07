fafa27750f5d1d2413254ac408cf01c57556dc53
Fernando Lozano <flozano@redhat.com>
2016-01-07 15:02:18 -0500

    cleanup of php_api CORS code

--- apps/html5/compile.sh
+++ apps/html5/compile.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-sudo systemctl start httpd24-httpd
-#sudo systemctl enable httpd24-httpd
-
 DOCROOT=/opt/rh/httpd24/root/var/www/html/
 
 sudo rm -rf $DOCROOT/todo
--- apps/php_api/todo/api/db.php
+++ apps/php_api/todo/api/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$dsn = 'mysql:host='.$_ENV["MYSQL_PORT_3306_TCP_ADDR"] . ':' . $_ENV["MYSQL_PORT_3306_TCP_PORT"] .';dbname='.$_ENV["MYSQL_ENV_MYSQL_DATABASE"];
-$user = $_ENV["MYSQL_ENV_MYSQL_USER"];
-$pass = $_ENV["MYSQL_ENV_MYSQL_PASSWORD"];
+$dsn = 'mysql:host='.getenv(MYSQL_PORT_3306_TCP_ADDR) . ':' . getenv(MYSQL_PORT_3306_TCP_PORT) .';dbname='.getenv(MYSQL_ENV_MYSQL_DATABASE);
+$user = getenv(MYSQL_ENV_MYSQL_USER);
+$pass = getenv(MYSQL_ENV_MYSQL_PASSWORD);
 
 ?>
