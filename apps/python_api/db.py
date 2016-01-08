e8576e7ca1795d31a3f54d70887c8d9d6b7faeea
zgutterm@redhat.com <you@example.com>
2016-01-08 09:58:54 -0500

    update env var names for python. fix routing issue

--- apps/python_api/db.py
+++ apps/python_api/db.py
@@ -2,9 +2,9 @@ import os
 
 
 class db:
-    username = os.environ.get("MYSQL_ENV_MYSQL_USER", 'user1')
-    password = os.environ.get("MYSQL_ENV_MYSQL_PASSWORD", 'mypa55')
-    host = os.environ.get("MYSQL_PORT_3306_TCP_ADDR", '127.0.0.1')
-    port = os.environ.get("MYSQL_PORT_3306_TCP_PORT", '3306')
-    name = os.environ.get("MYSQL_DB_NAME", 'items')
+    username = os.environ.get("MYSQL_ENV_MYSQL_USER")
+    password = os.environ.get("MYSQL_ENV_MYSQL_PASSWORD")
+    host = os.environ.get("MYSQL_PORT_3306_TCP_ADDR")
+    port = os.environ.get("MYSQL_PORT_3306_TCP_PORT")
+    name = os.environ.get("MYSQL_ENV_MYSQL_DATABASE")
 
